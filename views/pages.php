2dd3f98975ea0bd22d91b7c5326f825044e78af5
Delino <64547069+d3lino@users.noreply.github.com>
2021-07-02 11:07:28 +0100

    change desc size function, style card, ajout modal

--- views/pages.php
+++ views/pages.php
@@ -63,7 +63,7 @@
 				<div class="col-8">
 					<div class="card-body">
 					<h5 class="card-title"><?= $title ?></h5>
-					<p class="card-text"><?= $desc ?></p>
+					<p class="card-text"><?= $descCard . ' <span class="more-info" data-bs-toggle="modal" data-bs-target="#exampleModal">Plus d\'infos</span>' ?></p>
 					<p class="card-text"><small class="text-muted"><i class="bi bi-clock"></i> <?= $time ?></small></p>
 					</div>
 				</div>
@@ -71,6 +71,25 @@
 		</div>
 
     <?php } ?>
+
+	<!-- Modal -->
+	<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+				</div>
+				<div class="modal-body">
+					...
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+					<button type="button" class="btn btn-primary">Save changes</button>
+				</div>
+			</div>
+		</div>
+	</div>
 
 	<footer class="footer fixed-bottom d-flex flex-column align-items-center">
 		<div class="row">
